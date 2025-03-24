406293a2739e2401fb28d4010d91a9d098344c63
tpemartin <tpemartin@gmail.com>
2025-03-24 03:19:13 +0000

    update summary upload gs

--- travel-destination/reshape/reshaped_travel_data/parsing_summary/summary_gsheets_upload.R
+++ travel-destination/reshape/reshaped_travel_data/parsing_summary/summary_gsheets_upload.R
@@ -15,7 +15,8 @@ frequency_table_細分 <- summary_results$細分$Frequency_Table
 sheet_url <- "https://docs.google.com/spreadsheets/d/1tbwc7zuVcg3V72DzjFU8F19E7nZ_dsfjYP1ICiqcRcY/edit?gid=544763957"
 
 # Write each data frame to the specified ranges
-sheet_write(data = year_df, ss = sheet_url, sheet = "reshaped_travel_data_single_variable_summary")
+sheet_add(ss = sheet_url, sheet="reshaped_travel_data_single_variable_summary")
+range_write(data = year_df, ss = sheet_url, sheet = "reshaped_travel_data_single_variable_summary", range="A2")
 range_write(data = number_of_travelers_df, ss = sheet_url, sheet = "reshaped_travel_data_single_variable_summary", range = "A5:G6")
 range_write(data = frequency_table_首站抵達地, ss = sheet_url, sheet = "reshaped_travel_data_single_variable_summary", range = "I:J")
 range_write(data = frequency_table_細分, ss = sheet_url, sheet = "reshaped_travel_data_single_variable_summary", range = "L:M")
